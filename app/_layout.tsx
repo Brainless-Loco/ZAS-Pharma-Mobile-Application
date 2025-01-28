f1b3d9c06e0c410ee01c6b04b3a5f112e75257b2
Brainless-Loco <dev.tcdas@gmail.com>
2025-01-28 21:17:42 +0600

    Minor Edit on light mood

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,10 @@
 import { Stack } from 'expo-router';
+import { useEffect } from 'react';
+import { Appearance } from 'react-native';
 
 export default function Layout() {
+  useEffect(() => Appearance.setColorScheme('light'),
+  [])
   return (
     <Stack
       screenOptions={{
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,24 +3,32 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
+import { BACKGROUND_COLOR, TEXT_COLOR } from "@/components/ui/CustomColor";
+
 const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: TEXT_COLOR,
+    background: BACKGROUND_COLOR,
     tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
+    // text: '#ECEDEE',
+    // background: '#151718',
+    // tint: tintColorDark,
+    // icon: '#9BA1A6',
+    // tabIconDefault: '#9BA1A6',
+    // tabIconSelected: tintColorDark,
+    text: TEXT_COLOR,
+    background: BACKGROUND_COLOR,
+    tint: tintColorLight,
+    icon: '#687076',
+    tabIconDefault: '#687076',
+    tabIconSelected: tintColorLight,
   },
 };
